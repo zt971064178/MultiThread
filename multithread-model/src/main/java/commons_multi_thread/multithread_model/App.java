a5f32a2c7a54f4998a43dd8ddea7787f8ed80acf
zhangtian <zhangtian@DESKTOP-JF9M0VD>
2016-07-05 12:51:53 +0800

    多线程经典模型

---
+++ multithread-model/src/main/java/commons_multi_thread/multithread_model/App.java
@@ -0,0 +1,13 @@
+package commons_multi_thread.multithread_model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
